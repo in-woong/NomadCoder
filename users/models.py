8564765da94f86497ea14f12612ffc7534ffcde4
inwoong <inwoong100@gmail.com>
2021-04-17 11:13:06 +0900

    #4

--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
 
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
-    birthdate = models.DateField(blank=True)
+    birthdate = models.DateField(null=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
 
