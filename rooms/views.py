a885c2d80bd57c5d6f08c088ee3da7c34a0e5900
inwoong <inwoong100@gmail.com>
2021-04-24 12:06:12 +0900

    #11.5

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,19 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.get_page(page)
-    return render(
-        request,
-        "rooms/home.html",
-        {"pages": rooms},
-    )
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            {"pages": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")
