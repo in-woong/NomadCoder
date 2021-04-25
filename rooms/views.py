169b5224664381d5834a8fdeef9e6f0eced2cbfc
inwoong <inwoong100@gmail.com>
2021-04-25 21:02:36 +0900

    #11.8

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,21 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            {"pages": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 10
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
