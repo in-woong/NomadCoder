a1eb20fa195f7e57b96cdec6fd97884fb6f26e1b
woong <inwoong100@gmail.com>
2022-04-11 22:33:38 +0900

    4.11 수강

--- cryptotracker/src/routes/Coin.tsx
+++ cryptotracker/src/routes/Coin.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useState } from 'react';
+import { useParams, useLocation } from 'react-router';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 50px;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  font-size: 50px;
+  display: block;
+`;
+
+interface RouteState {
+  state: {
+    name: string;
+  };
+}
 
 function Coin() {
+  const [loading, setLoading] = useState(false);
   const { coinId } = useParams<{ coinId: string }>();
-  return <h1>Coin : {coinId}</h1>;
+  const { state } = useLocation() as RouteState;
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인 {state?.name || 'Loading'}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
+
 export default Coin;
--- cryptotracker/src/theme.ts
+++ cryptotracker/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  textColor: 'white',
-  bgColor: 'black',
-  btnColor: 'teal',
+  textColor: '#bdc3c7',
+  bgColor: '#f39c12',
+  accentColor: '#2ecc71',
 };
