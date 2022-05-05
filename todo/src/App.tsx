b2475bced91685d080cff3a1576348a763009549
woong <inwoong100@gmail.com>
2022-05-05 23:40:24 +0900

    5.5 lecture

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import ToDolist from './ToDoList';
+import ToDolist from './componenets/ToDoList';
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
