1a8f779dda8845b9cf6775be1715c839199e8882
woong <inwoong100@gmail.com>
2022-12-02 00:22:41 +0900

    12.1 custom App

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,28 +1,33 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import path from 'path';
-import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav className={styles.nav}>
-      <Link
-        className={[
-          router.pathname === '/' ? styles.active : '',
-          styles.link,
-        ].join(' ')}
-        href='/'
-      >
-        Home
+    <nav>
+      <Link href='/'>
+        <span className={router.pathname === '/' ? 'active' : ''}>Home</span>
       </Link>
-      <Link
-        className={router.pathname === '/about' ? styles.active : ''}
-        href='/about'
-      >
-        About
+      <Link href='/about'>
+        <span className={router.pathname === '/about' ? 'active' : ''}>
+          About
+        </span>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+
+        span {
+          text-decoration: none;
+        }
+
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
