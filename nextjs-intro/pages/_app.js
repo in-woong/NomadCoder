d068581a6a02617483eb922d9fbb836bfc6bfc86
woong <inwoong100@gmail.com>
2022-12-04 23:03:56 +0900

    12.4 Serverside Rendering

--- nextjs-intro/pages/_app.js
+++ nextjs-intro/pages/_app.js
@@ -1,17 +1,12 @@
-import NavBar from '../components/NavBar';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 export default function CustomApp({ Component, pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-          text-decoration: none;
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
