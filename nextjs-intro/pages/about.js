faf295157f859f5b51164bda6b0d6c6deb4fb88a
woong <inwoong100@gmail.com>
2022-12-04 11:18:27 +0900

    12.4 introduction nextjs

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -3,13 +3,7 @@ import NavBar from '../components/NavBar';
 export default function Potate() {
   return (
     <div>
-      <NavBar />
       <h1>About</h1>
-      <style jsx global>{`
-        .active {
-          color: white;
-        }
-      `}</style>
     </div>
   );
 }
