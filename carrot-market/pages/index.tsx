5f77b621feed38a7e76982971d73659bf40726e1
woong <inwoong100@gmail.com>
2022-12-05 22:22:48 +0900

    4.10 Responsive Modifiers part Two

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='grid min-h-screen gap-10 bg-slate-400 py-20 px-5'>
-      <div className='flex flex-col rounded-3xl bg-white p-6 shadow-xl sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400'>
+    <div className='grid min-h-screen gap-10 bg-slate-400 py-20 px-5 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center'>
+      <div className='flex flex-col justify-between rounded-3xl bg-white p-6 shadow-xl '>
         <span className='text-3xl font-semibold'>Select Item</span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
         </button>
       </div>
       <div className='group overflow-hidden rounded-2xl  bg-white shadow-xl'>
-        <div className='bg-blue-500 p-6 pb-14'>
+        <div className='portrait:bg-indigo-blue bg-blue-500 p-6 pb-14 xl:pb-40 landscape:bg-teal-500'>
           <span className='text-2xl text-white'>Profile</span>
         </div>
         <div className='relative -top-5 rounded-3xl bg-white p-6'>
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className='rounded-3xl bg-white p-6 shadow-xl'>
+      <div className='rounded-3xl bg-white p-6 shadow-xl lg:col-span-2 xl:col-span-1'>
         <div className='mb-5 flex items-center justify-between'>
           <span>⬅️</span>
           <div className='space-x-3'>
