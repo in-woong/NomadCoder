f1cfe034bd7fd86a215ac0a8224cbe41bdfaaacd
woong <inwoong100@gmail.com>
2022-12-05 20:02:35 +0900

    4.2 Test Drive part Two

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
+      <div className='overflow-hidden rounded-2xl bg-white  shadow-xl'>
+        <div className='bg-blue-500 p-6 pb-14'>
+          <span className='text-2xl text-white'>Profile</span>
+        </div>
+        <div className='relative -top-5 rounded-3xl bg-white p-6'>
+          <div className='relative -top-16 flex items-end justify-between'>
+            <div className='flex flex-col items-center'>
+              <span className='text-small text-gray-500'>Orders</span>
+              <span className='font-medium'>340</span>
+            </div>
+            <div className='h-24 w-24 rounded-full bg-red-400'></div>
+            <div className='flex flex-col items-center'>
+              <span className='text-small text-gray-500'>Spent</span>
+              <span className='font-medium'>$2,310</span>
+            </div>
+          </div>
+          <div className='relative -mt-10 -mb-5 flex flex-col items-center'>
+            <span className='text-lg font-medium'>TonyMolloy</span>
+            <span className='text-sm text-gray-500'>New York, USA</span>
+          </div>
+        </div>
+      </div>
       <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
       <div className='rounded-2xl bg-white p-10 shadow-xl'></div>
     </div>
