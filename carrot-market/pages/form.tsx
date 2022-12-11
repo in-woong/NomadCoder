8478f0e2693f3b637081103077956fbf985eea6a
woong <inwoong100@gmail.com>
2022-12-11 12:41:05 +0900

    7.3 Validation

--- carrot-market/pages/form.tsx
+++ carrot-market/pages/form.tsx
@@ -3,21 +3,29 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function Forms() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("I'm valid");
+  };
+  const inValid = (e) => {
+    console.log(e);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, inValid)}>
       <input
-        {...register('username')}
+        {...register('username', { required: true })}
         type='text'
         placeholder='Username'
-        required
       />
-      <input {...register('email')} type='email' placeholder='Email' required />
       <input
-        {...register('password')}
+        {...register('email', { required: true })}
+        type='email'
+        placeholder='Email'
+      />
+      <input
+        {...register('password', { required: true })}
         type='password'
         placeholder='Password'
-        required
       />
       <input type='submit' value='Create Account' />
     </form>
