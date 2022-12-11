b4eac74c076ef32bf4decce6a0e372c1aa816347
woong <inwoong100@gmail.com>
2022-12-11 12:56:33 +0900

    7.4 Validation part Two

--- carrot-market/pages/form.tsx
+++ carrot-market/pages/form.tsx
@@ -1,29 +1,40 @@
-import { resizeImage } from 'next/dist/server/image-optimizer';
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import { FieldErrors, useForm } from 'react-hook-form';
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
-    console.log("I'm valid");
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log(data);
   };
-  const inValid = (e) => {
-    console.log(e);
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
   return (
-    <form onSubmit={handleSubmit(onValid, inValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register('username', { required: true })}
+        {...register('username', {
+          required: 'Username is required',
+          minLength: {
+            message: 'The username should be longer than 5 chars',
+            value: 5,
+          },
+        })}
         type='text'
         placeholder='Username'
       />
       <input
-        {...register('email', { required: true })}
+        {...register('email', { required: 'Email is required' })}
         type='email'
         placeholder='Email'
       />
       <input
-        {...register('password', { required: true })}
+        {...register('password', { required: 'Password is required' })}
         type='password'
         placeholder='Password'
       />
