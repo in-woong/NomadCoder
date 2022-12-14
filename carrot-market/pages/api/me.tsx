bb1cba1bcc2f59c5572e907bea7b29d1843a677f
woong <inwoong100@gmail.com>
2022-12-15 01:21:41 +0900

    9.8 Profile Handler

--- carrot-market/pages/api/me.tsx
+++ carrot-market/pages/api/me.tsx
@@ -1,5 +1,5 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
 
@@ -15,21 +15,14 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: { payload: token },
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
   });
-  if (!exists) {
-    return res.status(404).end();
-  }
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({ ok: true, profile });
 }
 
-export default withIronSessionApiRoute(withHandler('POST', handler), {
+export default withIronSessionApiRoute(withHandler('GET', handler), {
   cookieName: 'carrotsession',
   password: process.env.COOKIE_PASSWORD!,
 });
