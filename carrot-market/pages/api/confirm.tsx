b30badbec28f82b805cadb9866b276bb5f18337d
woong <inwoong100@gmail.com>
2022-12-15 01:02:03 +0900

    9.7 Serverless Sessions

--- carrot-market/pages/api/confirm.tsx
+++ carrot-market/pages/api/confirm.tsx
@@ -1,12 +1,27 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
 import withHandler from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@libs/server/client';
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: { payload: token },
+  });
+  if (!exists) {
+    return res.status(404).end();
+  }
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
-export default withHandler('POST', handler);
+
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrotsession',
+  password: process.env.COOKIE_PASSWORD!,
+});
