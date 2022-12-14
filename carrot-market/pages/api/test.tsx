84db58c9f50da0a71ab21f79094f511285472779
woong <inwoong100@gmail.com>
2022-12-14 21:03:32 +0900

    9.4 Sending SMS

--- carrot-market/pages/api/test.tsx
+++ carrot-market/pages/api/test.tsx
@@ -1,6 +1,9 @@
+import twilio from 'twilio';
 import client from '@libs/server/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '../../libs/server/withHandler';
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -23,6 +26,13 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `로그인 토큰은 ${payload}입니다.`,
+    });
+  }
 
   return res.json({ ok: true });
 }
