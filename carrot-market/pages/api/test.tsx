631fbe222949a0ec14abc10b23310f0c2033094d
woong <inwoong100@gmail.com>
2022-12-14 19:50:14 +0900

    9.2 Accounts Logic

--- carrot-market/pages/api/test.tsx
+++ carrot-market/pages/api/test.tsx
@@ -1,8 +1,21 @@
+import client from '@libs/server/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '../../libs/server/withHandler';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
