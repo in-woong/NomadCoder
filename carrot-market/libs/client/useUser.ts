bc1176d4fa34de965d64d764f6d17f1c933d90a0
woong <inwoong100@gmail.com>
2022-12-15 10:45:38 +0900

    10.3 SWR

--- carrot-market/libs/client/useUser.ts
+++ carrot-market/libs/client/useUser.ts
@@ -1,18 +1,12 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR('/api/me', fetcher);
   const router = useRouter();
-  useEffect(() => {
-    fetch('/api/me').then((response) =>
-      response.json().then((data) => {
-        if (!data.ok) {
-          return router.replace('/enter');
-        }
-        setUser(data.profile);
-      })
-    );
-  }, [router]);
-  return user;
+
+  return data;
 }
