271829f9552a6e45ddecc3d2f577ea695d498468
woong <inwoong100@gmail.com>
2022-12-18 23:08:47 +0900

    13.1 Reviews

--- carrot-market/pages/api/reviews.ts
+++ carrot-market/pages/api/reviews.ts
@@ -7,11 +7,26 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session.user);
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
   });
-  res.json({ ok: true, profile });
+
+  return res.json({ ok: true, reviews });
 }
 
 export default withApiSession(
