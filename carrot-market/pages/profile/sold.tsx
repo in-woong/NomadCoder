6355ce167341daa4f5bcfb0645c9a59a8f4b0489
woong <inwoong100@gmail.com>
2022-12-19 00:18:59 +0900

    13.4 Sales, Purchases, Favorites

--- carrot-market/pages/profile/sold.tsx
+++ carrot-market/pages/profile/sold.tsx
@@ -1,21 +1,26 @@
 import { NextPage } from 'next';
 import Item from '@components/item';
 import Layout from '@components/layout';
+import useSWR from 'swr';
+import { ProductWithCount } from 'pages';
+import ProductList from '@components/product-list';
+
+interface Record {
+  id: number;
+  product: ProductWithCount;
+}
+
+interface ProductListResponse {
+  [key: string]: Record[];
+}
 
 const Sold: NextPage = () => {
+  const { data } = useSWR<ProductListResponse>(`/api/users/me/sales`);
+
   return (
     <Layout canGoBack title='판매내역'>
       <div className='flex flex-col space-y-5 divide-y pb-10'>
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            id={i}
-            key={i}
-            title='iPhone 14'
-            price={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
+        <ProductList kind='sales' />
       </div>
     </Layout>
   );
