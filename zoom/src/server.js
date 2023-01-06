161c99ccb7a4dca98483caf1c5a8316d970c4b0e
woong <inwoong100@gmail.com>
2023-01-06 11:47:55 +0900

    1.4 / Websocket Message

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,3 +1,5 @@
+import WebSocket from 'ws';
+import http from 'http';
 import express from 'express';
 
 const app = express();
@@ -6,8 +8,21 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-app.get('/', (req, res) => res.render('home'));
-const haneldListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, haneldListen);
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server });
+
+wsServer.on('connection', (socket) => {
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'));
+  socket.on('message', (message) => {
+    console.log(message);
+  });
+  socket.send('hello!!!');
+});
+
+server.listen(3000, handleListen);
