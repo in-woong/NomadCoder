35adb58890561943ade1addfb321df82af294dc0
woong <inwoong100@gmail.com>
2023-01-11 10:59:25 +0900

    2.7 Nicknames and codeChallenge

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -2,16 +2,57 @@ const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
+const room = document.getElementById('room');
 
-function backendDone() {
-  console.log('backenddone');
+room.hidden = true;
+let roomName, nickName;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector('#msg');
+  msgForm.addEventListener('submit', handleMessageSubmit);
+}
+
+function addMessage(messages) {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = messages;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector('#msg input');
+  const value = input.value;
+  input.value = '';
+  socket.emit('new_message', value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector('input');
-  socket.emit('enter_room', input.value, backendDone);
-  input.value = '';
+  const roomInput = form.querySelector('#roomname');
+  const nameInput = form.querySelector('#nickname');
+  roomName = roomInput.value;
+  nickName = nameInput.value;
+  roomInput.value = '';
+  nameInput.value = '';
+
+  socket.emit('enter_room', roomName, nickName, showRoom);
 }
 
 form.addEventListener('submit', handleRoomSubmit);
+
+socket.on('welcome', (user) => {
+  addMessage(`${user} joined!`);
+});
+
+socket.on('bye', (user) => {
+  addMessage(`${user} left!`);
+});
+
+socket.on('new_message', addMessage);
