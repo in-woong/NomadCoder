167fdc94cebc641fe24a5cf1b6e579546c3a0d9b
woong <inwoong100@gmail.com>
2023-01-18 15:38:11 +0900

    3.5 IceCandidate

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -5,12 +5,20 @@ const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
 
+const call = document.getElementById('call');
+const welcome = document.getElementById('welcome');
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+let myPeerConnection;
+
 async function getCameras() {
   try {
+    ``;
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === 'videoinput');
     const currentCamera = myStream.getVideoTracks()[0];
@@ -80,8 +88,112 @@ async function handleCameraChange() {
   await getMedia(camerasSelect.value);
 }
 
-getMedia();
+const welcomeForm = welcome.querySelector('form');
+
+let roomName;
+let nickName;
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  const h2 = call.querySelector('h2');
+  h2.innerText = `Room ${roomName}`;
+
+  makeConnection();
+}
+
+function addMessage(messages) {
+  const ul = call.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = messages;
+  ul.appendChild(li);
+}
+
+async function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const roomNameInput = welcomeForm.querySelector('#roomname');
+  const nickNameInput = welcomeForm.querySelector('#nickname');
+
+  roomName = roomNameInput.value;
+  roomNameInput.value = '';
+
+  nickName = nickNameInput.value;
+  nickNameInput.value = '';
+
+  await initCall();
+  socket.emit('enter_room', roomName, nickName);
+}
+
+//socket Code
+
+socket.on('welcome', async (user, newCount) => {
+  const h2 = call.querySelector('h2');
+  h2.innerText = `Room ${roomName} (${newCount})`;
+  addMessage(`${user} joined!`);
+
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  socket.emit('offer', offer, roomName);
+  console.log('sent offer');
+});
+
+socket.on('bye', (user, newCount) => {
+  const h2 = call.querySelector('h2');
+  h2.innerText = `Room ${roomName} (${newCount})`;
+  addMessage(`${user} left!`);
+});
+
+socket.on('room_change', (rooms) => {
+  const roomList = welcome.querySelector('ul');
+  roomList.innerHTML = '';
+  if (!rooms.length) {
+    roomList.innerHTML = '';
+    return;
+  }
+});
+
+socket.on('offer', async (offer) => {
+  console.log('received offer');
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+  console.log('sent the answer');
+});
+
+socket.on('answer', (answer) => {
+  console.log('received the  answer');
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+  console.log('received candidate');
+  myPeerConnection.addIceCandidate(ice);
+});
 
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
 camerasSelect.addEventListener('input', handleCameraChange);
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+//RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener('icecandidate', handleIce);
+  myPeerConnection.addEventListener('addstream', handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log('got and sent iceCandidate');
+  socket.emit('ice', data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peersStream = document.querySelector('#peerFace');
+  peersStream.srcObject = data.stream;
+}
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -13,5 +13,62 @@ app.get('/*', (_, res) => res.redirect('/'));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on('connection', (socket) => {
+  socket['nickname'] = 'Annon';
+
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
+  });
+
+  socket.on('enter_room', (roomName, nickName) => {
+    socket['nickname'] = nickName;
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit('room_change', publicRooms());
+  });
+
+  socket.on('offer', (offer, roomName) => {
+    socket.to(roomName).emit('offer', offer);
+  });
+
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
+  });
+
+  socket.on('ice', (ice, roomName) => {
+    socket.to(roomName).emit('ice', ice);
+  });
+
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
+  });
+});
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
