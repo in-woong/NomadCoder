532772c8bfd979b356e7024807e0d416b3a7074f
woong <inwoong100@gmail.com>
2023-01-26 12:31:07 +0900

    feat: 4.6 interpolation

---
+++ ReactNative/langApp/App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, Easing, Pressable, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+export default function App() {
+  const [up, setUp] = useState(false);
+  const Y_POSITION = useRef(new Animated.Value(200)).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(Y_POSITION, {
+      toValue: up ? -300 : 300,
+      useNativeDriver: true,
+      duration: 5000,
+    }).start(toggleUp);
+  };
+
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, -100, 100, 300],
+    outputRange: [1, 0, 0, 1],
+  });
+
+  return (
+    <Container>
+      <Pressable onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            opacity: opacity,
+            transform: [{ translateY: Y_POSITION }],
+          }}
+        />
+      </Pressable>
+    </Container>
+  );
+}
