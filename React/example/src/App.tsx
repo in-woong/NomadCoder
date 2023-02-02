072baf52527f3cfd3d6f24736bddbacf30b67c0e
woong <inwoong100@gmail.com>
2023-02-02 11:47:59 +0900

    #7.1 set selectors

--- React/example/src/App.tsx
+++ React/example/src/App.tsx
@@ -1,7 +1,33 @@
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hourSelector, minuteState } from './atoms';
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(Number(e.currentTarget.value));
+  };
+
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(Number(e.currentTarget.value));
+  };
+
   return (
     <div className='App'>
-      <input type='number' placeholder='Minutes' />
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type='number'
+        placeholder='Minutes'
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type='number'
+        placeholder='Hours'
+      />
     </div>
   );
 }
