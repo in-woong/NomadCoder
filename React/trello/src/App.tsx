97ca5a6304c12592402d50099d570b45870c0eb3
woong <inwoong100@gmail.com>
2023-02-03 11:36:38 +0900

    7.7 Performance

--- React/trello/src/App.tsx
+++ React/trello/src/App.tsx
@@ -1,5 +1,13 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from 'react-beautiful-dnd';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
+import { todoState } from './atoms';
+import DraggableCard from './components/DraggableCard';
 
 const Wrapper = styled.div`
   display: flex;
@@ -25,17 +33,17 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
-const toDos = ['a', 'b', 'c', 'd', 'e', 'f'];
-
 function App() {
-  const onDragEnd = () => {};
+  const [todos, setTodos] = useRecoilState(todoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+    setTodos((oldTodos) => {
+      const copyTodos = [...oldTodos];
+      copyTodos.splice(source.index, 1);
+      copyTodos.splice(destination.index, 0, draggableId);
+      return copyTodos;
+    });
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
@@ -43,18 +51,8 @@ function App() {
           <Droppable droppableId='one'>
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                {todos.map((toDo, index) => (
+                  <DraggableCard key={index} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
               </Board>
