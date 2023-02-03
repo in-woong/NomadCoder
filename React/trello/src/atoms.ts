e1837bb98c74d9ac73f562b73a00b92d06fac2ea
woong <inwoong100@gmail.com>
2023-02-03 11:36:38 +0900

    7.7 Performance

--- React/trello/src/atoms.ts
+++ React/trello/src/atoms.ts
@@ -1,1 +1,6 @@
 import { atom } from 'recoil';
+
+export const todoState = atom({
+  key: 'toto',
+  default: ['a', 'b', 'c', 'd', 'e', 'f'],
+});
---
+++ React/trello/src/components/DraggableCard.tsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDraggableCard {
+  index: number;
+  toDo: string;
+}
+
+const DraggableCard = ({ index, toDo }: IDraggableCard) => {
+  console.log('Card Rendered', toDo);
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+};
+
+export default memo(DraggableCard);
