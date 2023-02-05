e7b4a0465e7e922448d354cc0b21d8f1d53865a9
woong <inwoong100@gmail.com>
2023-02-05 12:45:05 +0900

    #8.2 Basic Animations

---
+++ React/animation/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box
+        transition={{
+          type: 'spring',
+          delay: 0.5,
+          // damping: 3,
+          mass: 5,
+          bounce: 0.8,
+        }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
+    </Wrapper>
+  );
+}
+
+export default App;
