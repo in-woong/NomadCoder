ce2d291c17fbefb16f09b3a10c07c288671b518a
woong <inwoong100@gmail.com>
2023-02-06 11:34:50 +0900

    #8.14 You Need to Watch This

--- React/animation/src/App.tsx
+++ React/animation/src/App.tsx
@@ -6,24 +6,26 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
-
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 const boxVariants = {
@@ -49,33 +51,13 @@ function App() {
   const [showing, setShowing] = useState(false);
   const [visible, setVisible] = useState(1);
   const [isBack, setBack] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-  const nextBox = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 1 : prev + 1));
-  };
-  const prevBox = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 0 ? 10 : prev - 1));
-  };
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence mode='wait' custom={isBack}>
-        <Box
-          drag
-          dragSnapToOrigin
-          variants={boxVariants}
-          custom={isBack}
-          initial='entry'
-          animate='center'
-          exit='exit'
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextBox}>next</button>
-      <button onClick={prevBox}>prev</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>{clicked ? <Circle layoutId='one' /> : null}</Box>
+      <Box>{!clicked ? <Circle layoutId='one' /> : null}</Box>
     </Wrapper>
   );
 }
