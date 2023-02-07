1aa072e28f2d37c0474449a71b195d81b3a73e82
woong <inwoong100@gmail.com>
2023-02-07 11:36:45 +0900

    #9.4 Header part

---
+++ React/nomplix/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Tv';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/tv' element={<Tv />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
