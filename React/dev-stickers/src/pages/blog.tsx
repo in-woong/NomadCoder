59a560c2e31df18ff372648eb5872fb2f50953a3
woong <inwoong100@gmail.com>
2023-02-09 10:14:55 +0900

    #10.9 useStaticQuery

--- React/dev-stickers/src/pages/blog.tsx
+++ React/dev-stickers/src/pages/blog.tsx
@@ -1,14 +1,14 @@
 import { HeadFC } from 'gatsby';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
 export default function Blog() {
-  return (
-    <Layout title='Blog'>
-      <h1>Blog</h1>
-    </Layout>
-  );
+  const [blogPosts, setBlogPosts] = useState();
+  useEffect(() => {
+    fetchBlogPosts();
+  }, []);
+  return <Layout title='Blog'>{blogPosts}</Layout>;
 }
 
 export const Head: HeadFC = () => <Seo title='Blog' />;
